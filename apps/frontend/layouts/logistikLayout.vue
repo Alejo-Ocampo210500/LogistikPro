700c6c9006588f29c532eafa4dfe617c146dcb71
Alejo-Ocampo210500 <alejoangel210500@gmail.com>
2026-08-19 20:27:04 -0500

    pages configuracion

--- apps/frontend/layouts/logistikLayout.vue
+++ apps/frontend/layouts/logistikLayout.vue
@@ -1,4 +1,27 @@
+<!-- layouts/blank.vue -->
 <template>
-
+  <v-app dark class="logistik-app">
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
 </template>
-</script>>
+
+<script>
+export default {
+  name: 'BlankLayout'
+}
+</script>
+
+<style>
+html,
+body,
+#__nuxt,
+#__layout,
+.v-application,
+.v-application--wrap,
+.v-main,
+.v-main__wrap {
+  background: #07162d !important;
+}
+</style>
